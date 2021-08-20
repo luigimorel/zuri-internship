f7714fdba55bb13127ed36216f95b672a34b50e5
Luigi Morel <morelmiles@gmail.com>
2021-08-20 16:32:09 +0300

    Changes

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/morelmiles/zuri-internship
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/mail.v2 v2.3.1
+)
--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/morelmiles/zuri-internship/luigi"
+	gomail "gopkg.in/mail.v2"
 )
 
 var tpl *template.Template
+
+type Content struct {
+		FirstName string
+		SecondName string
+		Email string
+		Message	string
+		Reason string
+	}
 
 func init () {
 	tpl = template.Must(template.ParseGlob("templates/*.gohtml" ))
@@ -17,56 +28,48 @@ func main() {
 	luigi.LuigiMorel()
 
 
-// Task 2: Resume 
-http.HandleFunc("/", index)
-http.HandleFunc("/process", processor)
-http.ListenAndServe(":8080", nil)	
-}
+// Task 2: Resumel=
+r := mux.NewRouter()
+r.HandleFunc("/", index)
+r.HandleFunc("/process", process)}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	tpl.ExecuteTemplate(w, "index.gohtml", nil)
 }
 
-func processor(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-	return 
-}	
+func send(w http.ResponseWriter, r *http.Request) {
+	 m := &Content{
+	 Email: r.FormValue("emailAddress"),
+	FirstName : r.FormValue("firstName"),
+	SecondName : r.FormValue("secondName"),
+	Reason: r.FormValue("reason"),
+	Message: r.FormValue("message"),
+	}
 
-	firstName := r.FormValue("firstName")
-		secondName := r.FormValue("secondName")
-		emailAddress := r.FormValue("emailAddress")
-		reason := r.FormValue("reason")
-		message := r.FormValue("message")
+	err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
+  http.Redirect(w, r, "/process", http.StatusSeeOther)
 
+}
 
-		type Content struct {
-		FirstName string
-		SecondName string
-		Email string
-		Message	string
-		Reason string
-		} {
-		FirstName: firstName, 
-		SecondName: secondName,
-		Email: emailAddress,
-		Reason: reason,
-		Message: message,
-		}
-			tpl.ExecuteTemplate(w, "process.gohtml", values)
-		}
+func process(w http.ResponseWriter, r *http.Request) {
+    tpl.ExecuteTemplate(w, "templates/process.gohtml", nil)
+}
+
 
 func (m *Content ) Deliver() {
-	m := gomain.NewMessage()
-	 m.SetHeader("From", "Luigi Morel <morelmiles@gmail.com>")
+	m := gomail.NewMessage()
+	m.SetHeader("From", "Luigi Morel <morelmiles@gmail.com>")
     m.SetHeader("To", "Luigi Morel <jsmith@gmail.com>")
     m.SetAddressHeader("reply-to", "m.Email")
     m.SetHeader("Subject", "Contact")
-    m.SetBody("text/html", "<b>First Name</b>: m.FirstName")
-	m.SetBody("text/html", "<b>Second Name</b>: m.SecondName")
-    m.SetBody("text/html", "<b>Email</b>: m.Email")
-    m.SetBody("text/html", "<b>Message</b>: m.Message")
-	m.SetBody("text/html", "<b>Reason</b>: m.Reason")
+
+    m.SetBody("text/html", fmt.Sprintf("<b>First Name</b>: %s", m.FirstName))
+	m.SetBody("text/html", fmt.Sprintf("<b>Second Name</b>: %s", m.SecondName))
+    m.SetBody("text/html", fmt.Sprintf("<b>Email</b>: %s", m.Email))
+	m.SetBody("text/html", fmt.Sprintf("<b>Reason</b>: %s", m.Reason))
+    m.SetBody("text/html", fmt.Sprintf("<b>Message</b>: %s", m.Message))
 
     deliver := gomail.NewDialer("smtp.gmail.com", 587, "morelmiles@gmail.com", "password")
     if err := d.DialAndSend(m); err != nil {
